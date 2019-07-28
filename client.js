3716cc15240460907c330ad2872400f9cd859325
pachevang <vangpache@gmail.com>
2019-07-28 12:51:14 -0500

    adds monthly total cost  calculations

--- client.js
+++ client.js
@@ -4,24 +4,37 @@ console.log('in js');
 
 $(document).ready(readyNow)
 
+let totalCost = 0;
+
 function readyNow() {
     $('.submitButton').on('click', addToTable);
-    //make a delete function event listener
+    //make a delete event listener
     $('.tableBody').on('click', '#employeeDelete', deleteThisRow);
 }
 
+function deleteThisRow() {
+    console.log('delete clicked!');
+    $(this).parent().parent().remove();
+}
 
 function addToTable() {
     //add input fields to table plus delete button
     $('.tableBody').append(
-        `<tr><td>`+ 
-        $('#firstNameInputField').val() +`</td><td>`+
-        $('#lastNameInputField').val() +`</td><td>` +
-        $('#idInputField').val() +`</td><td>` +
-        $('#titleInputField').val() +`</td><td>$` +
-        $('#annualSalaryInputField').val() +`</td><td>` +
+        `<tr><td>` +
+        $('#firstNameInputField').val() + `</td><td>` +
+        $('#lastNameInputField').val() + `</td><td>` +
+        $('#idInputField').val() + `</td><td>` +
+        $('#titleInputField').val() + `</td><td class="salaryAmount">$` +
+        $('#annualSalaryInputField').val() + `</td><td>` +
         `<button id="employeeDelete">Delete</button></td></tr>`);
-    
+
+
+
+    totalCost += $('#annualSalaryInputField').val() / 12;
+    console.log(totalCost);
+    $('#monthlyTotalCostSpan').text(totalCost);
+
+
     //clear inputs
     $('#firstNameInputField').val('');
     $('#lastNameInputField').val('');
@@ -29,12 +42,6 @@ function addToTable() {
     $('#titleInputField').val('');
     $('#annualSalaryInputField').val('');
 
-    //calculate monthly costs total
-
-
 }
 
-function deleteThisRow() {
-    console.log('delete clicked!');
-    $(this).parent().parent().remove();
-}
+
